a1c16ef1a70fc5362931e9ae2580a9f7bf6ce7be
hliadchyshyn <9763795+hliadchyshyn@users.noreply.github.com>
2019-04-07 13:18:26 +0300

    Added files

---
+++ advanced_html_css/classwork/5.04 - preprocessors/gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+
+function style() {
+    return (
+        gulp
+            .src("./scss/*.scss")
+            .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+            .on("error", sass.logError)
+            .pipe(gulp.dest("./css"))
+    );
+}
+
+function watch(){
+    gulp.watch('./scss/*.scss', style)
+}
+exports.style = style;
+exports.watch = watch;
